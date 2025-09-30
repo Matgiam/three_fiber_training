e7693ee56cc92b4e9925c12a48f558a823cfc3dc
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-09-30 11:46:43 +0200

    init: created react project using vite and created basic file structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./index.css";
+
+function App() {
+	return (
+		<>
+			<div className="hello">
+				<h1>Hello world</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
